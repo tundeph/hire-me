e70dc74d578289d20b37eee5e9c67f0075ae43cd
tundeph <54335828+tundeph@users.noreply.github.com>
2022-01-04 19:30:19 +0100

    Integrating with Capacitor for iOS and Android apps

---
+++ android/app/src/main/java/com/hireme/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hireme.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hireme.app',
+  appName: 'hire-me',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
